2a64067670135b347c262f1b6010d72c79bb141f
yulin <yluin930414@gmail,com>
2025-05-15 16:12:02 +0800

    架構調整

--- app/src/main/java/com/example/movierecommendations/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/movierecommendations/ui/search/SearchFragment.kt
@@ -1,4 +1,4 @@
-package com.example.movierecommendations.ui.notifications
+package com.example.movierecommendations.ui.search
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.movierecommendations.databinding.FragmentNotificationsBinding
+import com.example.movierecommendations.databinding.FragmentDashboardBinding
 
-class NotificationsFragment : Fragment() {
+class SearchFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentDashboardBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+        val searchViewModel =
+            ViewModelProvider(this).get(SearchViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textDashboard
+        searchViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
--- app/src/main/java/com/example/movierecommendations/ui/search/SearchViewModel.kt
+++ app/src/main/java/com/example/movierecommendations/ui/search/SearchViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.movierecommendations.ui.dashboard
+package com.example.movierecommendations.ui.search
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class SearchViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is Search Fragment"
     }
     val text: LiveData<String> = _text
 }
